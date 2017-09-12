caf3be8a196542c138565919a1b1c950cf2995f6
Artem <artemvesnin1@gmail.com>
2017-09-12 15:17:16 +0700

    Язык программирования C # (часть 1) Массивы (4)

---
+++ 12.09.17.four/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double[] mas = { 10, 2, 3, 4, 5, 6, 2, 4, 1};
+			for (int i = 0; i < mas.Length - 1; i++)
+			{
+				for (int j = i + 1; j < mas.Length; j++)
+				{
+					if (mas[i] > mas[j])
+					{
+						double temp = mas[j];
+						mas[j]= mas[i];
+						mas[i] = temp;
+					}
+
+				}
+			}
+			foreach (double a in mas)
+			{
+				Console.WriteLine(a);
+					}
+
+		}
+	}
+}
