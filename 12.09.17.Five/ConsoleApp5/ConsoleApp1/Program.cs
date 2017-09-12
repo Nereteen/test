fd95ae184286246eeba9f6c114e46795714bca89
Artem <artemvesnin1@gmail.com>
2017-09-12 16:10:11 +0700

    Язык программирования C # (часть 1) Методы (5)

---
+++ 12.09.17.Five/ConsoleApp5/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Math.Add(x: 77, z:11));
+		}
+
+		class Math
+		{
+			public static double Add(double x, double y=33, double z = 33)
+			{
+				return x + y+z;
+			}
+		}
+
+
+	}
+}
