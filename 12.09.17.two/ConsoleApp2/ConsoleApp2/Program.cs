c2408d100912904eaaa68d2a42b9cc07c152f683
Artem <artemvesnin1@gmail.com>
2017-09-12 14:04:12 +0700

    Язык программирования C # (часть 1) Переменные(2)

---
+++ 12.09.17.two/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using myInt = System.Int32;
+namespace ConsoleApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			myInt i = 1; //присваиваем i=1
+			myInt e = 1;
+			i++; 
+			Double b = 3.14;
+			b--;
+			Double q = i;
+			Double w = 0;
+			Console.WriteLine(b/i); //выводим на экран
+			Console.WriteLine(q/w);
+
+			e = (int)b;
+			Console.WriteLine(b);
+			Console.WriteLine(e);
+
+			Color c = Color.White;
+			c++;
+			Console.WriteLine(c);
+			Console.WriteLine((int)c);
+			Mystruct s;
+			s.Age = 55;
+
+		}
+			
+	}
+	struct Mystruct
+		{
+		public myInt Age;
+	}
+	enum Color
+	{
+		Red,
+		Blue,
+		Green,
+		White=11,
+		Black
+				}
+}
---
+++ 12.09.17.two/ConsoleApp2/ConsoleApp2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ConsoleApp2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CtrlSoft")]
+[assembly: AssemblyProduct("ConsoleApp2")]
+[assembly: AssemblyCopyright("Copyright © CtrlSoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("0a6e7679-5a17-4940-a7f6-00f377ece149")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
