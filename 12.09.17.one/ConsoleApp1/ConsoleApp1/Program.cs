1a461f0b1abbbb24cb75b8e944f3ef65d23a4830
Artem <artemvesnin1@gmail.com>
2017-09-12 12:18:29 +0700

    Язык программирования C# (часть 1) Первый шаг

---
+++ 12.09.17.one/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class ramProg
+	{
+		static void Main(string[] args)
+		{
+			System.Console.WriteLine("Helloy First C# Programm");
+		}
+	}
+}
