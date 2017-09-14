5a2fd5329c5b318358f4fb952e9c41fd65e6eeff
Artem <artemvesnin1@gmail.com>
2017-09-14 15:00:55 +0700

    Язык программирования C # (часть 1) Объектно-ориентированное программирование в C# (7)

---
+++ 13.09.17.7/ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Node tree = new Node();
+			tree.SetData(5);
+			tree.SetLeft(new Node());
+			tree.SetRight(new Node());
+		}
+	}
+	class Node
+	{
+		int data;
+		Node left;
+		Node right;
+		public Node GetRight() { return right; }
+		public Node GetLeft() { return left; }
+		public int GetData() { return data; }
+		public void SetRight(Node n) { right = n; }
+		public void SetLeft(Node n) { left = n; }
+		public void SetData(int d) { data = d; }
+	}
+}
