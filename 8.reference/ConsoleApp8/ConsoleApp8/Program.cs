07b96a3866d12b5e26db38b7abe247d1e132757d
Artem <artemvesnin1@gmail.com>
2017-09-14 15:16:29 +0700

    Язык программирования C # (часть 1) ссылки (8)

---
+++ 8.reference/ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyClass myclass = new MyClass();
+			myclass.data = 10;
+			IncreasseData(myclass);
+			Console.WriteLine(myclass.data);
+		}
+		static void IncreasseData(MyClass c)
+		{
+			c.data++;
+		}
+	}
+	class MyClass
+	{
+		public int data;
+	}
+}
